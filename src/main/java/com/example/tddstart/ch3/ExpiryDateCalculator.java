7cf7762aba82e55d22dddae77a31cef6e0128c20
heeom <swimxdev@gmail.com>
2025-03-21 20:55:15 +0900

    feat : 상수 처리해서 테스트 통과하는 메서드 만들기

--- src/main/java/com/example/tddstart/ch3/ExpiryDateCalculator.java
+++ src/main/java/com/example/tddstart/ch3/ExpiryDateCalculator.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
+        if (payData.getBillingDate().equals(LocalDate.of(2025, 2, 28))) {
+            return LocalDate.of(2025, 3, 31);
+        }
         return payData.getBillingDate().plusMonths(1);
     }
 }
