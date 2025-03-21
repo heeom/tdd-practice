fd7e92311c05dbd4734b94ce5db7d63e6f1f45c4
heeom <swimxdev@gmail.com>
2025-03-21 20:46:12 +0900

    refactor : 모든 테스트 코드가 새로 만든 assertExpiryDate을 사용하도록 변경

--- src/test/java/com/example/tddstart/ch3/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tddstart/ch3/ExpiryDateCalculatorTest.java
@@ -12,28 +12,51 @@ public class ExpiryDateCalculatorTest {
     @Test
     @DisplayName("10000원을 납부하면, 한달 뒤가 서비스 만료일이 된다")
     public void pay_10000_then_expiry_date_should_be_1_month_later() {
-        assertExpiryDate(LocalDate.of(2025, 3, 19), BigDecimal.valueOf(10_000), LocalDate.of(2025, 4,19));
-        assertExpiryDate(LocalDate.of(2025, 4, 19), BigDecimal.valueOf(10_000), LocalDate.of(2025, 5,19));
+        assertExpiryDate(
+                new PayData(
+                        LocalDate.of(2025, 3, 19),
+                        BigDecimal.valueOf(10_000),
+                        LocalDate.of(2025, 3, 19)
+                ),
+                LocalDate.of(2025, 4, 19)
+        );
+        assertExpiryDate(
+                new PayData(
+                        LocalDate.of(2025, 4, 19),
+                        BigDecimal.valueOf(10_000),
+                        LocalDate.of(2025, 4, 19)
+                ),
+                LocalDate.of(2025, 5, 19)
+        );
     }
 
     @Test
     @DisplayName("납부일과 한달뒤 날짜가 같지 않음")
     public void billing_date_not_matched_with_1_month_later_date() {
         assertExpiryDate(
-                LocalDate.of(2025, 1, 31),
-                BigDecimal.valueOf(10_0000),
+                new PayData(
+                        LocalDate.of(2025, 1, 31),
+                        BigDecimal.valueOf(10_0000),
+                        LocalDate.of(2025, 1, 31)
+                ),
                 LocalDate.of(2025, 2, 28)
         );
 
         assertExpiryDate(
-                LocalDate.of(2025, 5, 31),
-                BigDecimal.valueOf(10_000),
+                new PayData(
+                        LocalDate.of(2025, 5, 31),
+                        BigDecimal.valueOf(10_000),
+                        LocalDate.of(2025, 5, 31)
+                ),
                 LocalDate.of(2025, 6, 30)
         );
 
         assertExpiryDate(
-                LocalDate.of(2024, 1, 31),
-                BigDecimal.valueOf(10_000),
+                new PayData(
+                        LocalDate.of(2024, 1, 31),
+                        BigDecimal.valueOf(10_000),
+                        LocalDate.of(2024, 1, 31)
+                ),
                 LocalDate.of(2024, 2, 29)
         );
     }
