34dd01c0a1fe815e54c34e512396c86c695aea4f
swim <swimxdev@gmail.com>
2025-04-02 20:37:54 +0900

    feat : Mockito, ArgumentCapture사용해서 인자 검증, 호출 검증

--- src/test/java/com/example/tddstart/ch7/UserRegisterTest.java
+++ src/test/java/com/example/tddstart/ch7/UserRegisterTest.java
@@ -1,20 +1,35 @@
 package com.example.tddstart.ch7;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
+@ExtendWith(MockitoExtension.class)
 public class UserRegisterTest {
     private UserRegister userRegister;
     private StubWeakPasswordChecker stubWeakPasswordChecker;
     private MemoryUserRepository memoryUserRepository;
     private SpyEmailNotifier emailNotifier;
+
+    @Mock
+    private WeakPasswordChecker weakPasswordCheckerMock;
+
+    @Mock
+    private EmailNotifier emailNotifierMock;
+
+    private UserRegister userRegisterMock = new UserRegister(weakPasswordCheckerMock, memoryUserRepository, emailNotifierMock);
+
 
     @BeforeEach
     void setup() {
@@ -22,6 +37,7 @@ public class UserRegisterTest {
         memoryUserRepository = new MemoryUserRepository();
         emailNotifier = new SpyEmailNotifier();
         userRegister = new UserRegister(stubWeakPasswordChecker, memoryUserRepository, emailNotifier);
+        userRegisterMock = new UserRegister(weakPasswordCheckerMock, memoryUserRepository, emailNotifierMock);
     }
 
     @DisplayName("약한 암호면 가입 실패")
@@ -83,5 +99,36 @@ public class UserRegisterTest {
         // 사용한 인자 검증
         String realEmail = captor.getValue();
         assertEquals("swim12@email.com", realEmail);
+    }
+
+    // 대역객체가 기대하는 대로 상호작용했는지 확인하는 것이 모의 객체의 주요 기능이다. -> Mockito를 이용하면 모의 객체가 기대한 대로 불렸는지 검증 가능하다.
+    @DisplayName("약한 암호인 경우 가입에 실패")
+    @Test
+    void whenWeakPasswordThenRegisterFail() {
+        given(weakPasswordCheckerMock.checkPassword("pw")).willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegisterMock.register("swim12", "pw","swim12@email.com");
+        });
+    }
+
+    @DisplayName("회원 가입시 암호검사 수행함")
+    @Test
+    void checkPassword() {
+        userRegisterMock.register("swim12", "password1","swim12@email.com");
+
+        then(weakPasswordCheckerMock).should().checkPassword(anyString());
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendMail() {
+        userRegisterMock.register("swim12", "password1","swim12@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class); // 모의 객체의 메서드를 호출할 때 전달한 객체를 담는 기능
+        then(emailNotifierMock).should() // 모의 객체의 메서드가 호출됐는지 확인할 때
+                .sendRegistrationEmail(captor.capture()); // 메서드를 호출할 떄 전달한 인자가 ArgumentCaptor에 담긴다
+
+        assertEquals("swim12@email.com", captor.getValue()); // 보관한 값은 getValue로 꺼내서 인자 검증시 사용할 수 있다
     }
 }
